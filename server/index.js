c5b433b0e05b5eae2c4f7c28efa76ad8ff7c510b
Ariful Islam <139483709+arif-islam12@users.noreply.github.com>
2025-06-19 04:18:37 +0600

    fix: remove trailing slash from CORS origin

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5001;
 const corsOptions = {
   origin: [
     'http://localhost:3000', // for local development
-    'https://bykorp.vercel.app/' // for your live website
+    'https://bykorp.vercel.app' // for your live website
   ],
   optionsSuccessStatus: 200
 };
